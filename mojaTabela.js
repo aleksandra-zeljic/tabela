004ecf43443e50b69f5f474a9e65e6f0a4cd1703
Aleksandra <aleksandrazeljic13@gmail.com>
2023-12-02 13:39:32 +0100

    refaktorisanje dodavanja elemenata u tabelu uz autoinkrement id-a

--- mojaTabela.js
+++ mojaTabela.js
@@ -1,8 +1,7 @@
-let keys = [//"id",
- "make", "model", "price", "year", "body", "fuel"]
-    let data = [
+let keys = ["id", "make", "model", "price", "year", "body", "fuel"]
+let data = [
         {
-            // "id": "1",
+            "id": "1",
             "make": "Audi",
             "model": "A4",
             "price": "15000",
@@ -11,7 +10,7 @@ let keys = [//"id",
             "fuel": "Diesel",
         },
         {
-            // "id": "2",
+            "id": "2",
             "make": "BMW",
             "model": "5",
             "price": "30000",
@@ -20,7 +19,7 @@ let keys = [//"id",
             "fuel": "Diesel",
         },
         {
-            // "id": "1",
+            "id": "3",
             "make": "Audi",
             "model": "A4",
             "price": "15000",
@@ -29,7 +28,7 @@ let keys = [//"id",
             "fuel": "Diesel",
         },
         {
-            // "id": "2",
+            "id": "4",
             "make": "BMW",
             "model": "5",
             "price": "30000",
@@ -38,80 +37,70 @@ let keys = [//"id",
             "fuel": "Diesel",
         }
     ]
+    
+
+incrementCounter = (function() {
+    let counter = data.length;
+    return function() {
+        counter++;
+        return counter;
+    }
+})();
 
 
 var tabela = document.createElement('table');
-for (var i = 0; i < data.length; i++){
-    var tr = document.createElement('tr');   
-
-    // var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var td4 = document.createElement('td');
-    var td5 = document.createElement('td');
-    var td6 = document.createElement('td');
-    var td7 = document.createElement('td');
-
-    // var text1 = document.createTextNode(data[i].id);
-    var text2 = document.createTextNode(data[i].make);
-    var text3 = document.createTextNode(data[i].model);
-    var text4 = document.createTextNode(data[i].price);
-    var text5 = document.createTextNode(data[i].year);
-    var text6 = document.createTextNode(data[i].body);
-    var text7 = document.createTextNode(data[i].fuel);
-
-    // td1.appendChild(text1);     tr.appendChild(td1);
-    td2.appendChild(text2);     tr.appendChild(td2);
-    td3.appendChild(text3);     tr.appendChild(td3);
-    td4.appendChild(text4);     tr.appendChild(td4);
-    td5.appendChild(text5);     tr.appendChild(td5);
-    td6.appendChild(text6);     tr.appendChild(td6);
-    td7.appendChild(text7);     tr.appendChild(td7);
-    
-    tabela.appendChild(tr);
-}
-
 var tabelala = document.getElementById('tabelala')
 tabelala.appendChild(tabela);
+tabelaOdNiza();
 
+
+function tabelaOdNiza(){
+    tabela.innerHTML = "";
+    for (var i = 0; i < data.length; i++){
+        var tr = document.createElement('tr');   
+
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var td5 = document.createElement('td');
+        var td6 = document.createElement('td');
+        var td7 = document.createElement('td');
+
+        var text1 = document.createTextNode(data[i].id);
+        var text2 = document.createTextNode(data[i].make);
+        var text3 = document.createTextNode(data[i].model);
+        var text4 = document.createTextNode(data[i].price);
+        var text5 = document.createTextNode(data[i].year);
+        var text6 = document.createTextNode(data[i].body);
+        var text7 = document.createTextNode(data[i].fuel);
+
+        td1.appendChild(text1);     tr.appendChild(td1);
+        td2.appendChild(text2);     tr.appendChild(td2);
+        td3.appendChild(text3);     tr.appendChild(td3);
+        td4.appendChild(text4);     tr.appendChild(td4);
+        td5.appendChild(text5);     tr.appendChild(td5);
+        td6.appendChild(text6);     tr.appendChild(td6);
+        td7.appendChild(text7);     tr.appendChild(td7);
+        
+        tabela.appendChild(tr);
+    }
+}
 
 const f = document.getElementById('addButton')
 
 f.addEventListener('click', function(){
 
-    var tabela = document.getElementById('tabelala')
-    var tr = document.createElement('tr');   
+    data.push({
+        "id" : incrementCounter(),
+        "make": document.querySelector('#make').value,
+        "model": document.querySelector('#model').value,
+        "price": document.querySelector('#price').value,
+        "year": document.querySelector('#year').value,
+        "body": document.querySelector('#body').value,
+        "fuel": document.querySelector('#fuel').value
+    });
 
-    // var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var td4 = document.createElement('td');
-    var td5 = document.createElement('td');
-    var td6 = document.createElement('td');
-    var td7 = document.createElement('td');
+    tabelaOdNiza();
 
-    let polje;
-    // const id = document.createTextNode('1')
-    polje = document.querySelector('#make')
-    const make = document.createTextNode(polje.value)
-    polje = document.querySelector('#model')
-    const model = document.createTextNode(polje.value)
-    polje = document.querySelector('#price')
-    const price = document.createTextNode(polje.value)
-    polje = document.querySelector('#year')
-    const year = document.createTextNode(polje.value)
-    polje = document.querySelector('#body')
-    const body = document.createTextNode(polje.value)
-    polje = document.querySelector('#fuel')
-    const fuel = document.createTextNode(polje.value)
-
-    // td1.appendChild(id);       tr.appendChild(td1);
-    td2.appendChild(make);     tr.appendChild(td2);
-    td3.appendChild(model);    tr.appendChild(td3);
-    td4.appendChild(price);    tr.appendChild(td4);
-    td5.appendChild(year);     tr.appendChild(td5);
-    td6.appendChild(body);     tr.appendChild(td6);
-    td7.appendChild(fuel);     tr.appendChild(td7);
-
-    tabela.appendChild(tr);
 })
